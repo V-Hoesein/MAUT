f82402198ac869b6a4882db0fc4264680c234575
Van Husein <70703161+AkbarHusein@users.noreply.github.com>
2024-11-03 14:57:38 +0700

    "Deleted migration files for creating 'tb_alternatif' and 'tb_mapel' tables."

--- database/migrations/2024_11_03_075119_create_tb_alternatif_table.php
+++ database/migrations/2024_11_03_075119_create_tb_alternatif_table.php
@@ -14,6 +14,12 @@ return new class extends Migration
         Schema::create('tb_alternatif', function (Blueprint $table) {
             $table->string('kode_alternatif')->index()->unique();
             $table->string('nama_alternatif');
+            $table->string('kelas'); // Ensure compatible type with `tb_kelas.kode_kelas`
+            $table->foreign('kelas')
+                ->references('kode_kelas')
+                ->on('tb_kelas')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
             $table->timestamps();
         });
     }
